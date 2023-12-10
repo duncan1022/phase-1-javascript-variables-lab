a483d2b8218adcfe89dd780d87270cf75a57fcc9
+ <+>
2023-12-10 17:08:12 -0500

    passing tests pt 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith'
+const companyName = "Scuber"
 
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
